b2860c64856ec693173d172b1815ffff968c5c5f
Vladimir Aguacunchi <marwladiagua2001@hotmail.com>
2024-11-06 15:41:37 -0500

    text highlighting function

--- src/pages/PDFPage.tsx
+++ src/pages/PDFPage.tsx
@@ -1,7 +1,9 @@
 import { ChangeEvent, useState } from "react";
 import "./PDFPage.css";
-import { getDocument,GlobalWorkerOptions } from "pdfjs-dist";
+import { getDocument,GlobalWorkerOptions, PDFDocumentProxy } from "pdfjs-dist";
+import { PDFDocument, rgb } from "pdf-lib";
 GlobalWorkerOptions.workerSrc="../../node_modules/pdfjs-dist/build/pdf.worker.mjs"
+
 export const PDFPage = () => {
   const [file, setFile] = useState<File>();
 
@@ -10,10 +12,48 @@ export const PDFPage = () => {
     if (target.files && target.files.length == 0) return;
     setFile(target.files?.[0]);
   };
+
+  const getContentPage=async(doc: PDFDocumentProxy,index:number)=>{
+    const page = await doc.getPage(index)
+    const content = await page.getTextContent();
+    return content; 
+  }
+
   const loadPDF = async () => {
+    
     const loadingTask=getDocument(URL.createObjectURL(file!))
-    const pdf = await loadingTask.promise
-    console.log(pdf);
+    const pdfjsDoc = await loadingTask.promise;
+    const pdfLibDoc = await PDFDocument.load(await file!.arrayBuffer()); 
+
+
+    const numPages = pdfjsDoc._pdfInfo.numPages;
+
+    const pageContentPromise:Promise<unknown>[]=[];
+    for (let index = 0; index < numPages; index++) {
+      pageContentPromise.push(getContentPage(pdfjsDoc,index+1))
+    }
+    const pageContent=await Promise.all(pageContentPromise)
+    
+    pageContent.map((page,index)=>{
+      const pdfLibPage = pdfLibDoc.getPage(index);
+      page.items.map((item)=>{
+        const { str, width, height, transform } = item;
+        const x = transform[4];
+        const y = transform[5];
+        pdfLibPage.drawRectangle({
+          x: x,
+          y: y,
+          width: width,
+          height: height,
+          color: rgb(1, 1, 0), 
+          opacity: 0.3,
+        });
+      })
+    })
+    const pdfBytes = await pdfLibDoc.save();
+    const blob = new Blob([pdfBytes], { type: "application/pdf" });
+    const url = URL.createObjectURL(blob);
+    window.open(url);
   };
   return (
     <div>
